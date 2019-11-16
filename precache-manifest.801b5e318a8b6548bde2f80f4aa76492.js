734376109dade24031056d8a02e04b8096e1fccd
konradge <gellerkonrad@gmail.com>
2019-11-16 10:35:57 +0100

    Updates

---
+++ precache-manifest.801b5e318a8b6548bde2f80f4aa76492.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8502588ba4efca4119dfdfcdb9b1a0b5",
+    "url": "/react-weather-app/index.html"
+  },
+  {
+    "revision": "0e762af2717dc7ad886c",
+    "url": "/react-weather-app/static/css/main.6959619c.chunk.css"
+  },
+  {
+    "revision": "1bec7116a022f8259e08",
+    "url": "/react-weather-app/static/js/2.c241ddfe.chunk.js"
+  },
+  {
+    "revision": "0e762af2717dc7ad886c",
+    "url": "/react-weather-app/static/js/main.ea4608db.chunk.js"
+  },
+  {
+    "revision": "c0028b91b573da9d8b5e",
+    "url": "/react-weather-app/static/js/runtime-main.93ecbe23.js"
+  }
+]);
